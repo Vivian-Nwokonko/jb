725aaf73cd921f917266579a3aa0739db1ec5dc4
VIV-PC <vivianezeobi.o@gmail.com>
2025-09-30 13:14:05 +0100

    added all

---
+++ JS/slider.js
@@ -0,0 +1,16 @@
+// js for owlcarousel slider in services on home page
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        slideBy: 2,
+        margin: 10,
+        autoplay: true,
+        items: 1,   //this is what makes the image display one at a time.
+
+
+        
+        
+    })
+})
+
+
